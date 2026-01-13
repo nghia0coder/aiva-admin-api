aeb2488c61430d97a6849dbe8e7e6a030ab57cc1
John <nghialmao@gmail.com>
2026-01-13 14:26:57 +0700

    feat: authorize for signalR

--- src/Aiva.Admin.Api.Web/Configurations/AuthConfig.cs
+++ src/Aiva.Admin.Api.Web/Configurations/AuthConfig.cs
@@ -15,6 +15,27 @@ public static class AuthConfig
     services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         .AddMicrosoftIdentityWebApi(configuration.GetSection("AppSettings:AzureAd"));
 
+    services.Configure<JwtBearerOptions>(JwtBearerDefaults.AuthenticationScheme, options =>
+    {
+      options.Events = new JwtBearerEvents
+      {
+        OnMessageReceived = context =>
+        {
+          var accessToken = context.Request.Query["access_token"];
+
+          // If the request is for our SignalR hub...
+          var path = context.HttpContext.Request.Path;
+          if (!string.IsNullOrEmpty(accessToken) &&
+              (path.StartsWithSegments("/hubs/conversation")))
+          {
+            // Read the token out of the query string
+            context.Token = accessToken;
+          }
+          return Task.CompletedTask;
+        }
+      };
+    });
+
     services.AddAuthorization();
 
     logger.LogInformation("Azure AD authentication configured");
